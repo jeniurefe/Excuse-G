80c973f97a8f88d49616a96af08779575cf640e9
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-05-13 04:54:22 +0000

    Falta poco

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My birg"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", " the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my luch",
+    "while I was playing"
+  ];
+
+  let whoIndex = 0;
+  let actionIndex = 0;
+  let whatIndex = 0;
+  let whenIndex = 0;
+
+  return who[whoIndex] + " " + action[actionIndex];
 };
